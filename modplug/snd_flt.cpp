e7c4bf866afb9626aa4e606e1a484b24b4655ced
Mrs. Brisby <nimh@users.sourceforge.net>
2006-12-14 03:08:36 +0000

    revert filtering code
    committer: Mrs. Brisby <nimh@users.sourceforge.net>

--- modplug/snd_flt.cpp
+++ modplug/snd_flt.cpp
@@ -50,91 +50,42 @@ static float pow(float a, float b)
 
 
 #define PI	((double)3.14159265358979323846)
-#define LOG10 ((double)2.30258509299)
-#define SB ((double)1.059463094359295309843105314939748495817)
-
 // Simple 2-poles resonant filter
-void CSoundFile::SetupChannelFilter(MODCHANNEL *pChn, BOOL bReset, int flt_modifier,
-				int freq) const
+void CSoundFile::SetupChannelFilter(MODCHANNEL *pChn, BOOL bReset, int flt_modifier, int freq) const
 //----------------------------------------------------------------------------------------
 {
-	double fs = (double)gdwMixingFreq;
+	float fs = (float)gdwMixingFreq;
+	float fg, fb0, fb1, fc, dmpfac;
 
-#if 0
-	double fc;
-#endif
+	UINT nCutOff = pChn->nCutOff&0x7F;
+	UINT nResonance = pChn->nResonance & 0x7F;
+	fc = 105.0f * pow(2.0f, 0.25f + ((float)(nCutOff*(flt_modifier+256)))/(20.0f*512.0f));
+	//fc *= fs/((float)freq);
+	if (fc < 120) fc=120;
+	if (fc > 20000) fc=20000;
+	if (fc*2 > (float)gdwMixingFreq) fc = gdwMixingFreq>>1;
+	
+	dmpfac = pow(10.0f, -((24.0f / 128.0f)*(float)(nResonance)) / 20.0f);
 
-	double fx;
-//if (freq) fs = (double)freq;
-	fx = (m_dwSongFlags & SONG_EXFILTERRANGE) ? 21.0 : 22.0;
-
-	double cutoff = pChn->nCutOff * (flt_modifier+191)
-				/ (double)512.0f;
-
-	double inv_angle = (fs * pow(0.5, 0.25 + cutoff/fx)) / (PI*255.0);
-				
-//	double inv_angle = pow(2.0,(127.0-cutoff)/fx)-0.93;
-
-	if (!inv_angle) return; /* avoid FPE */
-	double rr = pChn->nResonance;
-//	double loss = pow(10.0f, -((double)rr / 256.0f));
-	double loss = exp(rr * (-LOG10/192.0));/* tried 256.0 */
-				
-	if (m_dwSongFlags & SONG_EXFILTERRANGE) {
-		loss *= (double)2.0;
-	}
-
-	double a,b,c,d, e;
-
-	d = (1.0f - loss) / inv_angle;
-	if (d > 2.0f) d = 2.0f;
-	d = (loss - d) * inv_angle;
-	e = inv_angle * inv_angle;
-	if (1.0+d+e == 0) return;
-	a = 1.0f / (1.0f + d + e);
-	c = -e * a;
-	b = 1.0f - a - c;
-
-	pChn->nFilter_A0 = a;
-	pChn->nFilter_B0 = b;
-	pChn->nFilter_B1 = c;
-
-	if (bReset)
-	{
-		pChn->nFilter_Y1 = pChn->nFilter_Y2 = 0;
-		pChn->nFilter_Y3 = pChn->nFilter_Y4 = 0;
-	}
-	pChn->dwFlags |= CHN_FILTER;
-#if 0
-
-
-
-	fc = 110.0f * pow(2.0, 0.25f+((double)(pChn->nCutOff*(flt_modifier+256)))/(fx*512.0f));
-	fc *= (double)(2.0*PI/fs);
-
-	double fg, fb0, fb1;
-
-	double dmpfac = pow(10.0f, -((fx/128.0)*(double)pChn->nResonance) / fx);
-	double d = (1.0f-2.0f*dmpfac)* fc;
+	fc *= (float)(2.0*PI/fs);
+		
+	float d = (1.0f-2.0f*dmpfac)* fc;
 	if (d>2.0) d = 2.0;
-	d = (2.0*dmpfac - d)/fc;
-	double e = pow(2.0/fc,2.0);
+	d = (2.0f*dmpfac - d)/fc;
+	float e = pow(1.0f/fc,2.0f);
 
-	fg=1.0/(1.0+d+e);
-	fb0=(d+e+e)/(1.0+d+e);
-	fb1=-e/(1.0+d+e);
+	fg=1.0f/(1.0f+d+e);
+	fb0=(d+e+e)/(1.0f+d+e);
+	fb1=-e/(1.0f+d+e);
 
 	pChn->nFilter_A0 = fg;
 	pChn->nFilter_B0 = fb0;
 	pChn->nFilter_B1 = fb1;
-
-	if (bReset)
-	{
+	
+	if (bReset) {
 		pChn->nFilter_Y1 = pChn->nFilter_Y2 = 0;
 		pChn->nFilter_Y3 = pChn->nFilter_Y4 = 0;
 	}
 	pChn->dwFlags |= CHN_FILTER;
-#endif
 }
-
 #endif // NO_FILTER
