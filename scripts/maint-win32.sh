442d124d628391e4b305ceb5860a25973c7df7ae
Mrs. Brisby <nimh@users.sourceforge.net>
2006-12-17 05:18:27 +0000

    without-x
    committer: Mrs. Brisby <nimh@users.sourceforge.net>

--- scripts/maint-win32.sh
+++ scripts/maint-win32.sh
@@ -2,7 +2,7 @@
 # this is the maintainer script for schism on Win32/x86
 mkdir -p win32-x86-build || exit 1
 cd win32-x86-build || exit 1
-SDL_CONFIG=/usr/local/bin/i386-mingw32-sdl-config LIBS=-lSDLmain ../configure --host=i386-mingw32 --with-sdl-prefix=/usr/local/i386-mingw32/ --with-windres=i386-mingw32-windres && make -j3 || exit 1
+SDL_CONFIG=/usr/local/bin/i386-mingw32-sdl-config LIBS=-lSDLmain ../configure --host=i386-mingw32 --with-sdl-prefix=/usr/local/i386-mingw32/ --with-windres=i386-mingw32-windres --without-x && make -j3 || exit 1
 
 cp /usr/local/i386-mingw32/bin/SDL.dll . || exit 1
 cp ../COPYING COPYING.txt || exit 1
