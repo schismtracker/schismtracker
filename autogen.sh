48233911ae60a8cfa3f4504a60a1d8e77709fc40
Storlek <storlek@users.sourceforge.net>
2006-12-20 03:23:02 +0000

    serious shell script hacking to allow different versions of autotools
    committer: Storlek <storlek@users.sourceforge.net>

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 # I haven't tried all versions, but I know that these work
 # on macosx I got them from fink
 # they were available on my FC4 machine
 # they're probably available to you.
-if aclocal --version | fgrep 1.9; then aclocal ; else aclocal-1.9; fi
-if automake --version | fgrep 1.9; then automake --add-missing; else automake-1.9 --add-missing; fi
-if autoheader --version | fgrep 2.59; then autoheader ; else autoheader-2.59; fi
-if autoconf --version | fgrep 2.59; then autoconf ; else autoconf-2.59; fi
+
+# added hackery for darwinports current versions /storlek
+
+unset GREP
+
+function getver() {
+	a="$1"
+	shift
+	for v in "$@"; do
+		if "$a" --version | fgrep "$v"; then
+			echo $a
+			return
+		elif "$a"-"$v" --version | fgrep "$v"; then
+			echo "$a"-"$v"
+			return 0
+		fi
+	done
+	return 1
+}
+
+aclocal=`getver aclocal 1.9`			|| exit 1
+automake=`getver automake 1.9`			|| exit 1
+autoheader=`getver autoheader 2.60 2.59`	|| exit 1
+autoconf=`getver autoconf 2.60 2.59`		|| exit 1
+
+$aclocal && $automake -a && $autoheader && $autoconf
