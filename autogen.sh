4bb2018809a7777d46550e363e104d88d7363cce
Storlek <storlek@users.sourceforge.net>
2006-12-20 03:56:19 +0000

    milk
    committer: Storlek <storlek@users.sourceforge.net>

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
+# I haven't tried all versions, but I know that these work
+# on macosx I got them from fink
+# they were available on my FC4 machine
+# they're probably available to you.
+
+# added hackery for darwinports current versions /storlek
+
 unset GREP
 unset GREP_OPTIONS
 unset FGREP
 unset FGREP_OPTIONS
+
 getver() {
 	a="$1"
 	shift
+	if "$a" --version | fgrep "$v" >/dev/null; then
+		echo "$a"
+		echo "'$a' => $a version $v" 1>&2
+		return
+	fi
 	for v in "$@"; do
-		if "$a"-"$v" --version 2>/dev/null | fgrep "$v" >/dev/null 2>/dev/null; then
-			echo "echo '$a-$v' >&2"
+		elif "$a"-"$v" --version | fgrep "$v" >/dev/null; then
 			echo "$a"-"$v"
+			echo "'$a"-"$v' => $a version $v" 1>&2
 			return
 		fi
 	done
-	if "$a" --version 2>/dev/null | fgrep "$v" >/dev/null 2>/dev/null; then
-		echo "echo '$a-$v' >&2"
-		echo "$a"
-		return
-	fi
-	echo "echo '-- require $a-$v' >&2"
-	echo "exit 1"
 }
-(
-getver aclocal 1.9
-getver automake 1.9
-getver autoheader 2.60 2.59
-getver autoconf 2.60 2.59
-) | exec sh
+
+aclocal=`getver aclocal 1.9`
+automake=`getver automake 1.9`
+autoheader=`getver autoheader 2.60 2.59`
+autoconf=`getver autoconf 2.60 2.59`
+$aclocal && $automake -a && $autoheader && $autoconf
