63c5f8c6116cdfdb0e1043ec4179d137494ff0dd
Mrs. Brisby <nimh@users.sourceforge.net>
2006-12-27 03:15:57 +0000

    mu
    committer: Mrs. Brisby <nimh@users.sourceforge.net>

---
+++ scripts/maint-win32-fast.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# this is the maintainer script for schism on Win32/x86
+mkdir -p win32-x86-build || exit 1
+cd win32-x86-build || exit 1
+make -j3 || exit 1
+
+cp /usr/local/i386-mingw32/bin/SDL.dll . || exit 1
+cp ../COPYING COPYING.txt || exit 1
+cp ../sys/win32/schism.bat . || exit 1
+zip "Schism Tracker.zip" schismtracker.exe schism.bat SDL.dll COPYING.txt || exit 1
