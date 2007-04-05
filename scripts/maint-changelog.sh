f2abe367551019972651fede1291f5c27a760c0c
Mrs. Brisby <nimh@users.sourceforge.net>
2007-04-05 03:45:21 +0000

    broken machine
    committer: Mrs. Brisby <nimh@users.sourceforge.net>

--- scripts/maint-changelog.sh
+++ scripts/maint-changelog.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cvs2cl.pl -I 'build-version\.h' -f CVSChangeLog
-cat CVSChangeLog ~/import/schism/CVSChangeLog \
-	| ssh nimh@nimh.org 'cat > webshare/schism/CVSChangeLog.txt'
+#cat CVSChangeLog ~/import/schism/CVSChangeLog \
+#	| ssh nimh@nimh.org 'cat > webshare/schism/CVSChangeLog.txt'
+cat CVSChangeLog | ssh nimh@nimh.org 'cat > webshare/schism/CVSChangeLog.txt'
 
