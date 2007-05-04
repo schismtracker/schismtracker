0da015471e2ef3e1087167403911e4a8f36fbf9d
Mrs. Brisby <nimh@users.sourceforge.net>
2007-05-04 00:49:50 +0000

    wtf was this?
    committer: Mrs. Brisby <nimh@users.sourceforge.net>

--- scripts/maint-win32.sh
+++ scripts/maint-win32.sh
@@ -2,7 +2,7 @@
 # this is the maintainer script for schism on Win32/x86
 mkdir -p win32-x86-build || exit 1
 cd win32-x86-build || exit 1
-SDL_CONFIG=/usr/bin/i586-mingw32msvc-sdl-config LIBS=-lSDLmain ../configure --host=i586-mingw32msvc --with-sdl-prefix=/usr/i586-mingw32msvc/ --with-windres=i586-mingw32msvc-windres --without-x $@ && make -j3 || exit 1
+SDL_CONFIG=/usr/bin/i586-mingw32msvc-sdl-config LIBS=-lSDLmain ../configure --host=i586-mingw32msvc --with-sdl-prefix=/usr/i586-mingw32msvc/ --with-windres=i586-mingw32msvc-windres --disable-sdltest --without-x $@ && make -j3 || exit 1
 
 cp /usr/i586-mingw32msvc/bin/SDL.dll . || exit 1
 cp ../COPYING COPYING.txt || exit 1
