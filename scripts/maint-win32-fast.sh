2de90b70ea771ae37280e86d09a27bc148ee1dc7
Mrs. Brisby <nimh@users.sourceforge.net>
2007-06-13 02:53:25 +0000

    whoops
    committer: Mrs. Brisby <nimh@users.sourceforge.net>

--- scripts/maint-win32-fast.sh
+++ scripts/maint-win32-fast.sh
@@ -4,7 +4,7 @@ mkdir -p win32-x86-build || exit 1
 cd win32-x86-build || exit 1
 make -j3 || exit 1
 
-cp /usr/local/i386-mingw32/bin/SDL.dll . || exit 1
+cp /usr/i586-mingw32msvc/bin/SDL.dll . || exit 1
 cp ../COPYING COPYING.txt || exit 1
 cp ../sys/win32/schism.bat . || exit 1
 zip "Schism Tracker.zip" schismtracker.exe schism.bat SDL.dll COPYING.txt || exit 1
