ced9b12848f29f4a41d74dcac2441a1c1343fc25
Mrs. Brisby <nimh@users.sourceforge.net>
2007-06-18 20:11:13 +0000

    Build
    committer: Mrs. Brisby <nimh@users.sourceforge.net>

--- include/auto/build-version.h
+++ include/auto/build-version.h
@@ -1,3 +1,3 @@
 /* this file must be checked in before each proper build.
 */
-#define BUILD_VERSION "$Date: 2007/06/18 16:12:23 $ ($Revision: 1.78 $)"
+#define BUILD_VERSION "$Date: 2007/06/18 20:11:13 $ ($Revision: 1.79 $)"
