37e7c5bd5e7815a3ed45b985bb7adbba834fef5f
Mrs. Brisby <nimh@users.sourceforge.net>
2008-02-03 03:39:13 +0000

    autopackage calls this makepackage now
    committer: Mrs. Brisby <nimh@users.sourceforge.net>

--- scripts/maint-linux.sh
+++ scripts/maint-linux.sh
@@ -2,4 +2,4 @@
 # this is the maintainer script for schism on Linux/x86
 EXTRA_ARGS="$@"
 export EXTRA_ARGS
-exec makeinstaller sys/fd.org/autopackage.apspec
+exec makepackage sys/fd.org/autopackage.apspec
