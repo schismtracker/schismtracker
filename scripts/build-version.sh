f04f506f8edd226f64d9d737beb469a0f74f92ce
Mrs. Brisby <mrs.brisby@nimh.org>
2008-09-09 08:28:54 -0400

    Do this differently; use the checked-in version for display
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- scripts/build-version.sh
+++ scripts/build-version.sh
@@ -2,10 +2,10 @@
 if [ -d CVS ]; then
 	cvs ci -f -m 'Build' include/auto/build-version.h
 else
-	cd include/auto || exit 1
-	exec > build-version.h.tmp || exit 1
+	exec > include/auto/build-version.h.tmp || exit 1
 	echo "/* this file must be checked in before each proper build."
 	echo "*/"
-	date +'#define BUILD_VERSION "$Date: %Y/%m/%d %H:%M:%S $"' 
-	mv build-version.h.tmp build-version.h
+	date -r .git/refs/heads/master \
+		+'#define BUILD_VERSION "$Date: %Y/%m/%d %H:%M:%S $"' 
+	mv include/auto/build-version.h.tmp include/auto/build-version.h
 fi
