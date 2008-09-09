8a609052be79236db3706d9fd2b1e153a550589d
Mrs. Brisby <mrs.brisby@nimh.org>
2008-09-09 08:53:44 -0400

    cvs export on upload
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- scripts/maint-upload.sh
+++ scripts/maint-upload.sh
@@ -4,3 +4,4 @@ scp -C cvs-snapshot.tgz nimh@nimh.org:webshare/schism/.
 scp -C schism-0.5.x86.package nimh@nimh.org:webshare/schism/.
 scp -C "win32-x86-build/install.exe" nimh@nimh.org:webshare/schism/.
 scp -C "macosx-ppc-build/Schism Tracker.dmg" nimh@nimh.org:webshare/schism/.
+sh scripts/maint-cvsexport.sh
