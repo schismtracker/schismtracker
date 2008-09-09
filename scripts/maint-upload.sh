0fb69f09784112281e6c73c05d061db54807e808
Mrs. Brisby <mrs.brisby@nimh.org>
2008-09-09 08:58:59 -0400

    update dumb server
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- scripts/maint-upload.sh
+++ scripts/maint-upload.sh
@@ -5,3 +5,5 @@ scp -C schism-0.5.x86.package nimh@nimh.org:webshare/schism/.
 scp -C "win32-x86-build/install.exe" nimh@nimh.org:webshare/schism/.
 scp -C "macosx-ppc-build/Schism Tracker.dmg" nimh@nimh.org:webshare/schism/.
 sh scripts/maint-cvsexport.sh
+git-update-server-info
+scp .git/info/refs nimh@nimh.org:webshare/schism.git/info/refs
