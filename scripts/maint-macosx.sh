7b5902ae3ec45a3e2eb14e5013ae934baf62358b
Mrs. Brisby <mrs.brisby@nimh.org>
2008-09-09 08:42:24 -0400

    Fix here for fink
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- scripts/maint-macosx.sh
+++ scripts/maint-macosx.sh
@@ -5,10 +5,10 @@ HERE=`basename "$HERE"`
 # I use fink for SDL and stuff, and I don't have (regular) access to XCode
 mm=`cat "$HOME/.my-macosx"`
 (cd ..; rsync \
- --cvs-exclude --exclude="*-build" --exclude="schism-*.x86.package" --exclude="*.tgz" \
+ --cvs-exclude --exclude=.git --exclude="*-build" --exclude="schism-*.x86.package" --exclude="*.tgz" \
  -v -z -r -e ssh "$HERE" "$mm:.")
 
-ssh "$mm" 'cd "$HERE"; PATH="/sw/bin:$PATH"; export PATH; sh scripts/fink-macosx.sh'" $@" || exit 1
+ssh "$mm" 'cd "'"$HERE"'"; PATH="/sw/bin:$PATH"; export PATH; sh scripts/fink-macosx.sh'" $@" || exit 1
 
 mkdir -p macosx-ppc-build
 rsync -v -z -r -e ssh "$mm:$HERE/macosx-ppc-build/./" macosx-ppc-build/.
