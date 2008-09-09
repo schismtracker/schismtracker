e67e19c2e0871c7c77a3c62416633b43ec1cc097
Mrs. Brisby <mrs.brisby@nimh.org>
2008-09-09 08:50:06 -0400

    Update version
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- include/auto/build-version.h
+++ include/auto/build-version.h
@@ -1,3 +1,3 @@
 /* this file must be checked in before each proper build.
 */
-#define BUILD_VERSION "$Date: 2008/09/09 04:14:23 $ ($Revision: 1.239 $)"
+#define BUILD_VERSION "$Date: 2008/09/09 08:27:04 $"
