eb46a52f5d83818c9a32cf12d9ada3ec48498a4b
Mrs. Brisby <mrs.brisby@nimh.org>
2008-09-09 23:02:35 -0400

    fix this
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- scripts/maint-cvsexport.sh
+++ scripts/maint-cvsexport.sh
@@ -3,5 +3,5 @@ set -e
 export GIT_DIR="$HOME/src/schism/.git"
 cd "$HOME/src/schism2"
 git-cherry cvshead  | sed -n 's/^+ //p' | xargs -l1 git-cvsexportcommit -c -p -v
-git-branch -M cvshead
+git-branch --no-track -f cvshead
 git-update-server-info
