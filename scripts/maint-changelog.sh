4a1b9af63c6852bbe788b94f1499db9c3682f378
Mrs. Brisby <mrs.brisby@nimh.org>
2008-09-14 19:42:52 -0400

    ugh; Changelog is now my changelog
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- scripts/maint-changelog.sh
+++ scripts/maint-changelog.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 if [ -d CVS ]; then
-	cvs2cl -I 'build-version\.h' -f CVSChangeLog
+	cvs2cl -I 'build-version\.h' -f ChangeLog
 else
-	git log > CVSChangeLog
+	git log > ChangeLog
 fi
-cat CVSChangeLog | ssh nimh@nimh.org 'cat > webshare/schism/CVSChangeLog.txt'
-
+cat ChangeLog | ssh nimh@nimh.org 'cat > webshare/schism/CVSChangeLog.txt'
