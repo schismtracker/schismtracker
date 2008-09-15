162eac6f4c8eeb6e9b0d9e96620bee60efd08756
Mrs. Brisby <mrs.brisby@nimh.org>
2008-09-14 21:36:24 -0400

    eh
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- scripts/maint-upload.sh
+++ scripts/maint-upload.sh
@@ -4,6 +4,7 @@ scp -C cvs-snapshot.tgz nimh@nimh.org:webshare/schism/.
 scp -C schism-0.5.x86.package nimh@nimh.org:webshare/schism/.
 scp -C "win32-x86-build/install.exe" nimh@nimh.org:webshare/schism/.
 scp -C "macosx-ppc-build/Schism Tracker.dmg" nimh@nimh.org:webshare/schism/.
+git-update-server-info
 sh scripts/maint-cvsexport.sh
 scp .git/info/refs nimh@nimh.org:webshare/schism.git/info/refs
 scp .git/objects/info/packs nimh@nimh.org:webshare/schism.git/objects/info/packs
