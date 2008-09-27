d114adb0f807e8065c8a6cc7963388d7699aeecf
Joel Yliluoma <bisqwit@iki.fi>
2008-09-27 13:57:39 +0300

    Fix KEYOFF for AdLib & MIDI (no longer acts like a KEYCUT)
    committer: Joel Yliluoma <joel.yliluoma@iki.fi>

--- modplug/sndmix.cpp
+++ modplug/sndmix.cpp
@@ -1025,11 +1025,13 @@ BOOL CSoundFile::ReadNote()
                     volume = GMVol[volume<0 ? 0 : volume>63 ? 63 : volume];
                     volume /= 2;
                 }
-                GM_SetFreqAndVol(nChn, freq, volume, BendMode);
+                GM_SetFreqAndVol(nChn, freq, volume, BendMode,
+                                 pChn->dwFlags & CHN_KEYOFF);
             }
             else if((pChn->dwFlags & CHN_ADLIB) && !(pChn->dwFlags & CHN_NOTEFADE)) 
 			{
-    			OPL_NoteOn(nChn, freq*2/3); // for some reason, scaling by 1.5 is needed.
+			    int oplfreq = freq*2/3; // for some reason, scaling by 1.5 is needed.
+    			OPL_HertzTouch(nChn, oplfreq, pChn->dwFlags & CHN_KEYOFF);
     			// ST32 ignores global & master volume in adlib mode, guess we should do the same -Bisqwit
                 OPL_Touch(nChn, (vol * pChn->nInsVol * 63 / (1<<20)));
             }
