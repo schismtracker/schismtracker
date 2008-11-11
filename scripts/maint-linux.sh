6fceb6018259d9d12ce1a8cb155c08df77b7f4b4
Mrs. Brisby <mrs.brisby@nimh.org>
2008-11-10 23:37:16 -0500

    blah. autopackage is broken, and they're making it worse...
    committer: Mrs. Brisby <mrs.brisby@nimh.org>

--- scripts/maint-linux.sh
+++ scripts/maint-linux.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # this is the maintainer script for schism on Linux/x86
-EXTRA_ARGS="$@"
-export EXTRA_ARGS
-exec makepackage sys/fd.org/autopackage.apspec
+mkdir -p linux-x86-build || exit 1
+cd linux-x86-build || exit 1
+../configure $@ && make -j3 || exit 1
+make
