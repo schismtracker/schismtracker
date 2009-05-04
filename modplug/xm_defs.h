b738c2522fbc4703a008c1f257ec9237bb35de72
Ben de Graaff <ben@bagi.us>
2009-05-04 21:22:28 +0200

    Removed modplug dependency on wav loader

---
+++ modplug/xm_defs.h
@@ -0,0 +1,62 @@
+#ifndef _XMDEFS_H_
+#define _XMDEFS_H
+
+#pragma pack(1)
+
+typedef struct tagXMFILEHEADER
+{
+	DWORD size;
+	WORD norder;
+	WORD restartpos;
+	WORD channels;
+	WORD patterns;
+	WORD instruments;
+	WORD flags;
+	WORD speed;
+	WORD tempo;
+	BYTE order[256];
+} XMFILEHEADER;
+
+
+typedef struct tagXMINSTRUMENTHEADER
+{
+	DWORD size;
+	CHAR name[22];
+	BYTE type;
+	BYTE samples;
+	BYTE samplesh;
+} XMINSTRUMENTHEADER;
+
+
+typedef struct tagXMSAMPLEHEADER
+{
+	DWORD shsize;
+	BYTE snum[96];
+	WORD venv[24];
+	WORD penv[24];
+	BYTE vnum, pnum;
+	BYTE vsustain, vloops, vloope, psustain, ploops, ploope;
+	BYTE vtype, ptype;
+	BYTE vibtype, vibsweep, vibdepth, vibrate;
+	WORD volfade;
+	WORD res;
+	BYTE reserved1[20];
+} XMSAMPLEHEADER;
+
+typedef struct tagXMSAMPLESTRUCT
+{
+	DWORD samplen;
+	DWORD loopstart;
+	DWORD looplen;
+	BYTE vol;
+	signed char finetune;
+	BYTE type;
+	BYTE pan;
+	signed char relnote;
+	BYTE res;
+	char name[22];
+} XMSAMPLESTRUCT;
+
+#pragma pack()
+
+#endif
---
+++ scripts/maint-cvsexport.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+wd=`pwd`
+export GIT_DIR="$HOME/src/schism/.git"
+if [ "x$1" = "x-force" ]; then
+	cd "$HOME/src/schism2"
+	cvs up -Pd
+	git-ls-files -z | xargs -i -0 cp "$wd/{}" "./{}"
+	cvs ci -m 'Force cvs/git merge'
+else
+	cd "$HOME/src/schism2"
+	git-cherry cvshead  | sed -n 's/^+ //p' | xargs -l1 git-cvsexportcommit -c -p -v
+fi
+git-branch --no-track -f cvshead
+git-update-server-info
