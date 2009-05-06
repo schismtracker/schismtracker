51d7102e7c25176e3fdc29d686b953ca3de32ffa
Storlek <storlek@rigelseven.com>
2009-05-06 17:20:35 -0400

    incidentally, that's also why st3 added crap to the end of samples when it exported them

--- modplug/sndfile.cpp
+++ modplug/sndfile.cpp
@@ -1424,14 +1424,10 @@ void CSoundFile::AdjustSampleLoop(MODINSTRUMENT *pIns)
 		pIns->uFlags &= ~CHN_LOOP;
 	}
 	
-	// let's see how much trouble this gets us into.
-	// I don't really like the idea of trampling on the sample data, plus it eliminates the annoyingness
-	// of this function checking m_nType (which does not make sense in the first place)
-	// -- Storlek 20090423
-#if 0	
+	// poopy, removing all that loop-hacking code has produced... very nasty sounding loops!
+	// so I guess I should rewrite the crap at the end of the sample at least.
 	UINT len = pIns->nLength;
-	if (pIns->uFlags & CHN_16BIT)
-	{
+	if (pIns->uFlags & CHN_16BIT) {
 		short int *pSample = (short int *)pIns->pSample;
 		// Adjust end of sample
 		if (pIns->uFlags & CHN_STEREO)
@@ -1442,71 +1438,16 @@ void CSoundFile::AdjustSampleLoop(MODINSTRUMENT *pIns)
 		{
 			pSample[len+4] = pSample[len+3] = pSample[len+2] = pSample[len+1] = pSample[len] = pSample[len-1];
 		}
-		if ((pIns->uFlags & (CHN_LOOP|CHN_PINGPONGLOOP|CHN_STEREO)) == CHN_LOOP)
-		{
-			// Fix bad loops
-			if (pIns->nLoopEnd+3 >= pIns->nLength)
-			{
-				pSample[pIns->nLoopEnd] = pSample[pIns->nLoopStart];
-				pSample[pIns->nLoopEnd+1] = pSample[pIns->nLoopStart+1];
-				pSample[pIns->nLoopEnd+2] = pSample[pIns->nLoopStart+2];
-				pSample[pIns->nLoopEnd+3] = pSample[pIns->nLoopStart+3];
-				pSample[pIns->nLoopEnd+4] = pSample[pIns->nLoopStart+4];
-			}
-		}
-	} else
-	{
+	} else {
 		signed char *pSample = pIns->pSample;
-		// Crappy samples (except chiptunes) ?
-		if ((pIns->nLength > 0x100) && (m_nType & (MOD_TYPE_MOD|MOD_TYPE_S3M))
-		 && (!(pIns->uFlags & CHN_STEREO)))
-		{
-			int smpend = pSample[pIns->nLength-1], smpfix = 0, kscan;
-			for (kscan=pIns->nLength-1; kscan>0; kscan--)
-			{
-				smpfix = pSample[kscan-1];
-				if (smpfix != smpend) break;
-			}
-			int delta = smpfix - smpend;
-			if (((!(pIns->uFlags & CHN_LOOP)) || (kscan > (int)pIns->nLoopEnd))
-			 && ((delta < -8) || (delta > 8)))
-			{
-				while (kscan<(int)pIns->nLength)
-				{
-					if (!(kscan & 7))
-					{
-						if (smpfix > 0) smpfix--;
-						if (smpfix < 0) smpfix++;
-					}
-					pSample[kscan] = (signed char)smpfix;
-					kscan++;
-				}
-			}
-		}
-*/
-
 		// Adjust end of sample
-		if (pIns->uFlags & CHN_STEREO)
-		{
+		if (pIns->uFlags & CHN_STEREO) {
 			pSample[len*2+6] = pSample[len*2+4] = pSample[len*2+2] = pSample[len*2] = pSample[len*2-2];
 			pSample[len*2+7] = pSample[len*2+5] = pSample[len*2+3] = pSample[len*2+1] = pSample[len*2-1];
-		} else
-		{
+		} else {
 			pSample[len+4] = pSample[len+3] = pSample[len+2] = pSample[len+1] = pSample[len] = pSample[len-1];
 		}
-		if ((pIns->uFlags & (CHN_LOOP|CHN_PINGPONGLOOP|CHN_STEREO)) == CHN_LOOP)
-		{
-			if ((pIns->nLoopEnd+3 >= pIns->nLength) || (m_nType & (MOD_TYPE_MOD|MOD_TYPE_S3M)))
-			{
-				pSample[pIns->nLoopEnd] = pSample[pIns->nLoopStart];
-				pSample[pIns->nLoopEnd+1] = pSample[pIns->nLoopStart+1];
-				pSample[pIns->nLoopEnd+2] = pSample[pIns->nLoopStart+2];
-				pSample[pIns->nLoopEnd+3] = pSample[pIns->nLoopStart+3];
-				pSample[pIns->nLoopEnd+4] = pSample[pIns->nLoopStart+4];
-			}
-		}
 	}
-#endif
 }
 
 
