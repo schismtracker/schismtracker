e7bfeec47ba56dc983884608693a7bdd0ca87aed
Storlek <storlek@rigelseven.com>
2009-05-08 12:25:29 -0400

    style cleanup; put multichannel indicators back

--- schism/pattern-view.c
+++ schism/pattern-view.c
@@ -26,6 +26,9 @@
 #include "song.h"
 #include "pattern-view.h"
 
+/* '^.' if given mask bit is set, otherwise '^' */
+#define INDICATOR(x) ((mask & (x)) ? 171 : 169)
+
 /* this stuff's ugly */
 
 /* --------------------------------------------------------------------- */
@@ -81,43 +84,31 @@ void draw_note_13(int x, int y, song_note * note, int cursor_pos, int fg,
         }
 }
 
-#define INDICATOR(x) ((mask & (x)) ? 171 : 169) /* '^.' if given mask bit is set, otherwise '^' */
-
 void draw_mask_13(int x, int y, int mask, int cursor_pos, int fg, int bg)
 {
 	char buf[16] = {170, 170, 170, 143, 143, 143, 143, 143, 143, 143, 143, 143, 143, 0};
+	char c = (cursor_pos > 0) ? 170 : 169;
 
-	if (cursor_pos == 0) {
-		/* draw note with '^.' */
-		buf[0] = buf[1] = buf[2] = 171;
+	if (mask & MASK_INSTRUMENT)
+		buf[4] = buf[5] = c;
+	if (mask & MASK_VOLUME)
+		buf[7] = buf[8] = c;
+	if (mask & MASK_EFFECT)
+		buf[10] = buf[11] = buf[12] = 169;
 
-		/* draw other masked fields with '^' */
-		if (mask & MASK_INSTRUMENT)
-			buf[4] = buf[5] = 169;
-		if (mask & MASK_VOLUME)
-			buf[7] = buf[8] = 169;
-		if (mask & MASK_EFFECT)
-			buf[10] = buf[11] = buf[12] = 169;
-	} else {
-		if (mask & MASK_INSTRUMENT)
-			buf[4] = buf[5] = 170;
-		if (mask & MASK_VOLUME)
-			buf[7] = buf[8] = 170;
-		if (mask & MASK_EFFECT)
-			buf[10] = buf[11] = buf[12] = 170;
-		switch (cursor_pos) {
-			case 1: buf[2] = 171; break;
+	switch (cursor_pos) {
+		case 0: buf[0] = buf[1] = buf[2] = 171; break;
+		case 1: buf[2] = 171; break;
 
-			case 2: buf[4] = INDICATOR(MASK_INSTRUMENT); break;
-			case 3: buf[5] = INDICATOR(MASK_INSTRUMENT); break;
+		case 2: buf[4] = INDICATOR(MASK_INSTRUMENT); break;
+		case 3: buf[5] = INDICATOR(MASK_INSTRUMENT); break;
 
-			case 4: buf[7] = INDICATOR(MASK_VOLUME); break;
-			case 5: buf[8] = INDICATOR(MASK_VOLUME); break;
+		case 4: buf[7] = INDICATOR(MASK_VOLUME); break;
+		case 5: buf[8] = INDICATOR(MASK_VOLUME); break;
 
-			case 6: buf[10] = INDICATOR(MASK_EFFECT); break;
-			case 7: buf[11] = INDICATOR(MASK_EFFECT); break;
-			case 8: buf[12] = INDICATOR(MASK_EFFECT); break;
-		}
+		case 6: buf[10] = INDICATOR(MASK_EFFECT); break;
+		case 7: buf[11] = INDICATOR(MASK_EFFECT); break;
+		case 8: buf[12] = INDICATOR(MASK_EFFECT); break;
 	}
 	draw_text(buf, x, y, fg, bg);
 }
