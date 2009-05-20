cd4a1bab3e9fe9d46a5182108e8edc49079bc683
Star <Storlek@users.noreply.github.com>
2009-05-19 22:56:58 -0400

    removed de

--- schism/fmt/mtm.c
+++ schism/fmt/mtm.c
@@ -1,4 +1,4 @@
-de/*
+/*
  * Schism Tracker - a cross-platform Impulse Tracker clone
  * copyright (c) 2003-2005 Storlek <storlek@rigelseven.com>
  * copyright (c) 2005-2008 Mrs. Brisby <mrs.brisby@nimh.org>
