2b6764a1bd485f4cf082761ad7fc95c6c3c4d437
Storlek <storlek@rigelseven.com>
2009-07-02 01:59:08 -0400

    fixed some insanity

--- schism/page_loadmodule.c
+++ schism/page_loadmodule.c
@@ -124,7 +124,7 @@ TODO: scroller hack on selected filename
 static char filename_entry[PATH_MAX + 1] = "";
 static char dirname_entry[PATH_MAX + 1] = "";
 
-char cfg_filename_pattern[PATH_MAX + 1] = GLOB_DEFAULT;
+char cfg_module_pattern[PATH_MAX + 1] = GLOB_DEFAULT;
 static char **glob_list = NULL;
 
 /* --------------------------------------------------------------------- */
@@ -438,7 +438,7 @@ static void set_glob(const char *globspec)
 
 static void set_default_glob(int set_filename)
 {
-	const char *s = cfg_filename_pattern;
+	const char *s = cfg_module_pattern;
 	if (set_filename) {
 		/* glob on load page is visible, but on save page the text should be empty */
 		strcpy(filename_entry, s);
