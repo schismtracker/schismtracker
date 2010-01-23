71120ae13b3d24914d1edcbba614d2997988b90f
Storlek <storlek@rigelseven.com>
2010-01-23 18:09:53 -0500

    cleaning some cobwebs

--- scripts/maint-release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-scp -C cvs-snapshot.tgz nimh@nimh.org:webshare/schism-release/.
-scp -C schism-0.5.x86.package nimh@nimh.org:webshare/schism-release/.
-scp -C "win32-x86-build/Schism Tracker.zip" nimh@nimh.org:webshare/schism-release/.
-scp -C "macosx-ppc-build/Schism Tracker.dmg" nimh@nimh.org:webshare/schism-release/.
