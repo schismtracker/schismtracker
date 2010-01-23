503f625eb2e11cc9d71746730c4d1278f1f84edc
Star <Storlek@users.noreply.github.com>
2010-01-23 18:09:53 -0500

    cleaning some cobwebs

--- scripts/maint-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sh scripts/build-version.sh
-sh scripts/maint-macosx.sh $@ &
-sh scripts/maint-linux.sh $@ &
-sh scripts/maint-win32.sh $@ &
-wait
-sh scripts/maint-tarball.sh
-sh scripts/maint-changelog.sh
--- scripts/maint-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# this is the maintainer script for schism on Linux/x86
-mkdir -p linux-x86-build || exit 1
-cd linux-x86-build || exit 1
-../configure $@ && make -j3 || exit 1
-make
