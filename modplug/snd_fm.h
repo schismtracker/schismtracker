302f4c3abc1ffd9d10552d661780370d4941d4f5
Storlek <storlek@rigelseven.com>
2010-02-01 00:16:11 -0500

    I still don't pretend to understand this fully, but I sure can line it up + due credit for the patch set

--- modplug/snd_fm.h
+++ modplug/snd_fm.h
@@ -59,19 +59,19 @@ void OPL_Close(void);
  *      Register numbers for the global registers
  */
 
-#define TEST_REGISTER                           0x01
+#define TEST_REGISTER                   0x01
 #define   ENABLE_WAVE_SELECT            0x20
 
-#define TIMER1_REGISTER                         0x02
-#define TIMER2_REGISTER                         0x03
-#define TIMER_CONTROL_REGISTER                  0x04    /* Left side */
+#define TIMER1_REGISTER                 0x02
+#define TIMER2_REGISTER                 0x03
+#define TIMER_CONTROL_REGISTER          0x04    /* Left side */
 #define   IRQ_RESET                     0x80
 #define   TIMER1_MASK                   0x40
 #define   TIMER2_MASK                   0x20
 #define   TIMER1_START                  0x01
 #define   TIMER2_START                  0x02
 
-#define CONNECTION_SELECT_REGISTER              0x04    /* Right side */
+#define CONNECTION_SELECT_REGISTER      0x04    /* Right side */
 #define   RIGHT_4OP_0                   0x01
 #define   RIGHT_4OP_1                   0x02
 #define   RIGHT_4OP_2                   0x04
@@ -79,15 +79,15 @@ void OPL_Close(void);
 #define   LEFT_4OP_1                    0x10
 #define   LEFT_4OP_2                    0x20
 
-#define OPL3_MODE_REGISTER                      0x05    /* Right side */
+#define OPL3_MODE_REGISTER              0x05    /* Right side */
 #define   OPL3_ENABLE                   0x01
 #define   OPL4_ENABLE                   0x02
 
-#define KBD_SPLIT_REGISTER                      0x08    /* Left side */
-#define   COMPOSITE_SINE_WAVE_MODE      0x80            /* Don't use with OPL-3? */
+#define KBD_SPLIT_REGISTER              0x08    /* Left side */
+#define   COMPOSITE_SINE_WAVE_MODE      0x80    /* Don't use with OPL-3? */
 #define   KEYBOARD_SPLIT                0x40
 
-#define PERCOSSION_REGISTER                     0xbd    /* Left side only */
+#define PERCUSSION_REGISTER             0xbd    /* Left side only */
 #define   TREMOLO_DEPTH                 0x80
 #define   VIBRATO_DEPTH                 0x40
 #define   PERCOSSION_ENABLE             0x20
@@ -103,31 +103,31 @@ void OPL_Close(void);
  *
  *      AM/VIB/EG/KSR/Multiple (0x20 to 0x35)
  */
-#define AM_VIB                                  0x20
+#define AM_VIB                          0x20
 #define   TREMOLO_ON                    0x80
 #define   VIBRATO_ON                    0x40
 #define   SUSTAIN_ON                    0x20
 #define   KSR                           0x10    /* Key scaling rate */
-#define   MULTIPLE_MASK         0x0f    /* Frequency multiplier */
+#define   MULTIPLE_MASK                 0x0f    /* Frequency multiplier */
 
  /*
   *     KSL/Total level (0x40 to 0x55)
   */
-#define KSL_LEVEL                               0x40
+#define KSL_LEVEL                       0x40
 #define   KSL_MASK                      0xc0    /* Envelope scaling bits */
 #define   TOTAL_LEVEL_MASK              0x3f    /* Strength (volume) of OP */
 
 /*
  *      Attack / Decay rate (0x60 to 0x75)
  */
-#define ATTACK_DECAY                            0x60
+#define ATTACK_DECAY                    0x60
 #define   ATTACK_MASK                   0xf0
 #define   DECAY_MASK                    0x0f
 
 /*
  * Sustain level / Release rate (0x80 to 0x95)
  */
-#define SUSTAIN_RELEASE                         0x80
+#define SUSTAIN_RELEASE                 0x80
 #define   SUSTAIN_MASK                  0xf0
 #define   RELEASE_MASK                  0x0f
 
@@ -142,15 +142,15 @@ void OPL_Close(void);
  *
  *      F-Number low bits (0xA0 to 0xA8).
  */
-#define FNUM_LOW                                0xa0
+#define FNUM_LOW                        0xa0
 
 /*
  *      F-number high bits / Key on / Block (octave) (0xB0 to 0xB8)
  */
-#define KEYON_BLOCK                                     0xb0
-#define   KEYON_BIT                             0x20
-#define   BLOCKNUM_MASK                         0x1c
-#define   FNUM_HIGH_MASK                        0x03
+#define KEYON_BLOCK                     0xb0
+#define   KEYON_BIT                     0x20
+#define   BLOCKNUM_MASK                 0x1c
+#define   FNUM_HIGH_MASK                0x03
 
 /*
  *      Feedback / Connection (0xc0 to 0xc8)
@@ -164,9 +164,9 @@ void OPL_Close(void);
  *      For 4 OP voices the connection bit is used in the
  *      both halves (gives 4 ways to connect the operators).
  */
-#define FEEDBACK_CONNECTION                             0xc0
-#define   FEEDBACK_MASK                         0x0e    /* Valid just for 1st OP of a voice */
-#define   CONNECTION_BIT                        0x01
+#define FEEDBACK_CONNECTION             0xc0
+#define   FEEDBACK_MASK                 0x0e    /* Valid just for 1st OP of a voice */
+#define   CONNECTION_BIT                0x01
 /*
  *      In the 4 OP mode there is four possible configurations how the
  *      operators can be connected together (in 2 OP modes there is just
@@ -201,8 +201,9 @@ void OPL_Close(void);
  *                                              |
  *                                      >--4----+
  */
-#define   STEREO_BITS                           0x30    /* OPL-3 only */
+#define   STEREO_BITS                   0x30    /* OPL-3 only */
 #define     VOICE_TO_LEFT               0x10
 #define     VOICE_TO_RIGHT              0x20
 
 #endif
+
