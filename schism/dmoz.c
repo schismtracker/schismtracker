17db8eb43da6e645f0dffc99ca431ef739b9808f
Storlek <storlek@rigelseven.com>
2010-02-03 07:39:00 -0500

    consolidating filetype lists

--- schism/dmoz.c
+++ schism/dmoz.c
@@ -80,101 +80,12 @@ static const char *devices[] = {
 /* --------------------------------------------------------------------------------------------------------- */
 /* file format tables */
 
-/* The type list should be arranged so that the types with the most specific checks are first, and the vaguest
-ones are down at the bottom. This is to ensure that some lousy type doesn't "steal" files of a different type.
-For example, if IT came before S3M, any S3M file starting with "IMPM" (which is unlikely, but possible, and in
-fact quite easy to do) would be picked up by the IT check. In fact, Impulse Tracker itself has this problem.
-
-Also, a format that might need to do a lot of work to tell if a file is of the right type (i.e. the MDL format
-practically requires reading through the entire file to find the title block) should be down farther on the
-list for performance purposes.
-
-Don't rearrange the formats that are already here unless you have a VERY good reason to do so. I spent a good
-3-4 hours reading all the format specifications, testing files, checking notes, and trying to break the
-program by giving it weird files, and I'm pretty sure that this ordering won't fail unless you really try
-doing weird stuff like hacking the files, but then you're just asking for trouble. ;) */
-
-#define READ_INFO(t) fmt_##t##_read_info
+#define READ_INFO(t) fmt_##t##_read_info,
 
 static const fmt_read_info_func read_info_funcs[] = {
-        /* 669 has lots of checks to compensate for a really crappy 2-byte magic. (It's even a common English
-        word ffs... "if"?!) Still, it's better than STM. The only reason this is first is because the position
-        of the SCRM magic lies within the 669 message field, and the 669 check is much more complex (and thus
-        more likely to be right). */
-        READ_INFO(669),
-
-        /* Since so many programs have added noncompatible extensions to the mod format, there are about 30
-        strings to compare against for the magic. Also, there are special cases for WOW files, which even
-        share the same magic as plain ProTracker, but are quite different; there are some really nasty
-        heuristics to detect these... ugh, ugh, ugh. However, it has to be above the formats with the magic
-        at the beginning... */
-        READ_INFO(mod),
-
-        /* S3M needs to be before a lot of stuff. */
-        READ_INFO(s3m),
-        /* FAR and S3M have different magic in the same place, so it doesn't really matter which one goes
-        where. I just have S3M first since it's a more common format. */
-        READ_INFO(far),
-
-        /* These next formats have their magic at the beginning of the data, so none of them can possibly
-        conflict with other ones. I've organized them pretty much in order of popularity. */
-        READ_INFO(xm),
-        READ_INFO(it),
-        READ_INFO(psm),
-        READ_INFO(mt2),
-        READ_INFO(mtm),
-        READ_INFO(ntk),
-#ifdef USE_NON_TRACKED_TYPES
-        READ_INFO(sid),  /* 6581 0wnz j00! */
-#endif
-        READ_INFO(mdl),
-        READ_INFO(med),
-        READ_INFO(okt),
-        READ_INFO(mus),
-        READ_INFO(mf),
-
-        READ_INFO(xi),
-
-        READ_INFO(iti),
-        READ_INFO(its),
-        READ_INFO(s3i),
-        READ_INFO(au),
-        READ_INFO(aiff),
-
-        READ_INFO(ult),
-        READ_INFO(liq),
-
-        READ_INFO(ams),
-        READ_INFO(f2r),
-        READ_INFO(dtm),  /* not sure about the placement here */
-
-        /* IMF and SFX (as well as STX) all have the magic values at 0x3C-0x3F, which is positioned in IT's
-        "reserved" field, Not sure about this positioning, but these are kind of rare formats anyway. */
-        READ_INFO(imf),
-        READ_INFO(sfx),
-
-        /* bleh */
-#if defined(USE_NON_TRACKED_TYPES) && defined(HAVE_VORBIS)
-        READ_INFO(ogg),
-#endif
-        READ_INFO(mid),
-        READ_INFO(pat),
-
-        /* STM seems to have a case insensitive magic string with several possible values, and only one byte
-        is guaranteed to be the same in the whole file... yeagh. */
-        READ_INFO(stm),
-
-        READ_INFO(wav),
-        /* An ID3 tag could actually be anywhere in an MP3 file, and there's no guarantee that it even exists
-        at all. I might move this toward the top if I can figure out how to identify an MP3 more precisely. */
-#ifdef USE_NON_TRACKED_TYPES
-        READ_INFO(mp3),
-#endif
-
+#include "fmt-types.h"
         NULL /* This needs to be at the bottom of the list! */
 };
-
-#undef READ_INFO
 
 /* --------------------------------------------------------------------------------------------------------- */
 /* "selected" and cache */
