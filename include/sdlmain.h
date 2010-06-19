3117c771fb5748f0408c3080471c3fd79edde3cc
Star <Storlek@users.noreply.github.com>
2010-06-18 21:03:04 -0400

    make opengl headers optional (for embedded systems) - thanks stuckie

--- include/sdlmain.h
+++ include/sdlmain.h
@@ -7,14 +7,6 @@
 
 /* just a fancy way to get SDL headers */
 
-#  ifndef _USE_AUTOCONF
-#    ifdef _SDL_IN_PATH
-#      undef _SDL_BY_PATH
-#    else
-#      define _SDL_BY_PATH
-#    endif
-#  endif
-
 #  ifdef USE_X11
 #    undef DISABLE_X11
 #    ifndef __unix__
@@ -22,18 +14,11 @@
 #    endif
 #  endif
 
-#  ifdef _SDL_BY_PATH
-#    include <SDL/SDL.h>
-#    include <SDL/SDL_thread.h>
-#    ifndef __cplusplus
-#      include <SDL/SDL_syswm.h>
-#      include <SDL/SDL_opengl.h>
-#    endif
-#  else
-#    include <SDL.h>
-#    include <SDL_thread.h>
-#    ifndef __cplusplus
-#      include <SDL_syswm.h>
+#  include <SDL.h>
+#  include <SDL_thread.h>
+#  ifndef __cplusplus
+#    include <SDL_syswm.h>
+#    ifdef USE_OPENGL
 #      include <SDL_opengl.h>
 #    endif
 #  endif
