69fdeb2997ad1daf93dd5d00b76a777d04dde5fd
Star <Storlek@users.noreply.github.com>
2010-10-03 14:22:30 -0400

    why was this checking cmwt?

--- fmt/it.c
+++ fmt/it.c
@@ -666,7 +666,7 @@ int fmt_it_load_song(song_t *song, slurp_t *fp, unsigned int lflags)
                    && hdr.msg_length == 0 && hdr.msg_offset == 0 && hdr.reserved == 0) {
                 // :)
                 tid = "OpenSPC conversion";
-        } else if ((hdr.cwtv >> 12) == 5 && hdr.cmwt == 0x0214) {
+        } else if ((hdr.cwtv >> 12) == 5) {
                 tid = (hdr.reserved == 0x54504d4f) ? "OpenMPT %d.%02x" : "OpenMPT %d.%02x (compat.)";
         } else if (hdr.cwtv == 0x0888 && hdr.cmwt == 0x0888 && hdr.reserved == 0/* && hdr.ordnum == 256*/) {
                 // erh.
