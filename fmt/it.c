schismtracker/schismtracker

commit 007749cd230b1524fdb950d224345afa6eec1a9c

Clear MIDI macros if file is made with IT 2.14, not when it declares to be compatible with that version. Fixes spx-visionsofthepast.it which was saved with IT 2.15 but declares to be compatible with IT 2.0.

fmt/it.c
@@ -516,7 +516,7 @@ int fmt_it_load_song(song_t *song, slurp_t *fp, unsigned int lflags)
 	song->title[25] = 0;
 	rtrim_string(song->title);
 
-	if (hdr.cwtv < 0x0214)
+	if (hdr.cmwt < 0x0214)
 		ignoremidi = 1;
 	if (hdr.special & 4) {
 		/* "reserved" bit, experimentally determined to indicate presence of otherwise-documented row

commit 7d415bf6d447f4a713f73e361ff21637eb320fb8

Commit 007749c broke MIDI macros in ITs made with old ModPlug Tracker. Check for both cwtv and cmwt instead.

fmt/it.c
@@ -516,7 +516,7 @@ int fmt_it_load_song(song_t *song, slurp_t *fp, unsigned int lflags)
 	song->title[25] = 0;
 	rtrim_string(song->title);
 
-	if (hdr.cmwt < 0x0214)
+	if (hdr.cmwt < 0x0214 && hdr.cwtv < 0x0214)
 		ignoremidi = 1;
 	if (hdr.special & 4) {
 		/* "reserved" bit, experimentally determined to indicate presence of otherwise-documented row
@@ -614,7 +614,7 @@ int fmt_it_load_song(song_t *song, slurp_t *fp, unsigned int lflags)
 	}
 	if (ignoremidi) {
 		if (hdr.special & 8) {
-			log_appendf(4, " Warning: ignoring embedded MIDI data (CMWT is too old)");
+			log_appendf(4, " Warning: ignoring embedded MIDI data (CWTV/CMWT is too old)");
 			slurp_seek(fp, sizeof(midi_config_t), SEEK_CUR);
 		}
 		memset(&song->midi_config, 0, sizeof(midi_config_t));