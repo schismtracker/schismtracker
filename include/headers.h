99ac6aacc019a77a38c8d54813a53e9c3b01daa6
Paper <paper@paper.us.eu.org>
2024-06-21 03:50:07 -0400

    sys/stdlib: add generic localtime_r replacement, remove win32-specific one
    All the win32 code did was put a little wrapper around localtime with a
    mutex. this does essentially the same thing but with SDL mutexes, so
    it works for basically any platform we can support.

--- include/headers.h
+++ include/headers.h
@@ -115,10 +115,7 @@ int asprintf(char **strp, const char *fmt, ...);
 int vasprintf(char **strp, const char *fmt, va_list ap);
 char *strptime(const char *buf, const char *fmt, struct tm *tm);
 int mkstemp(char *template);
-
-#ifdef SCHISM_WIN32
 struct tm *localtime_r(const time_t *timep, struct tm *result);
-#endif
 
 #define INT_SHAPED_PTR(v)               ((intptr_t)(void*)(v))
 #define PTR_SHAPED_INT(i)               ((void*)(i))
