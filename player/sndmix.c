45dea099ad9d48cb3e7948bccb5cc4b0f89055d2
Paper <paper@tflc.us>
2024-12-21 05:30:14 -0500

    player/sndmix: fix regressions within the new VU meter code

--- player/sndmix.c
+++ player/sndmix.c
@@ -668,21 +668,21 @@ static inline void rn_gen_key(song_t *csf, song_voice_t *chan, int32_t chan_num,
 static inline void update_vu_meter(song_voice_t *chan)
 {
 	// Update VU-Meter (final_volume is 14-bit)
+	uint32_t vutmp = chan->final_volume >> (14 - 8);
+	if (vutmp > 0xFF) vutmp = 0xFF;
 
-	if (chan->current_sample_data) {
-		// check mixer.c
-	} else if (chan->flags & CHN_ADLIB) {
+	// this check MUST be first
+	if (chan->flags & CHN_ADLIB) {
 		if (chan->strike>2)
 			chan->vu_meter=(0xFF*chan->final_volume)>>14;
 
 		// fake VU decay (intentionally similar to ST3)
 		chan->vu_meter = (chan->vu_meter > VUMETER_DECAY) ? (chan->vu_meter - VUMETER_DECAY) : 0;
 
-		if (chan->vu_meter >= 0x100) {
-			uint32_t vutmp = chan->final_volume >> (14 - 8);
-			if (vutmp > 0xFF) vutmp = 0xFF;
+		if (chan->vu_meter >= 0x100)
 			chan->vu_meter = vutmp;
-		}
+	} else if (vutmp && chan->current_sample_data) {
+		// check mixer.c
 	} else {
 		chan->vu_meter = 0;
 	}
