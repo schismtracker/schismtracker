7eba395528e28620236a2299cdf736c142bab9c7
Paper <paper@tflc.us>
2025-02-02 23:03:39 -0500

    CI/ubuntu: change install script destination, but add stub in /usr/local/bin

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -2,15 +2,51 @@
 # script to run schism with included libs on linux
 # shouldn't™ require bash or any other crap like that.
 
-# This is standard among GNU systems
-PREFIX="/usr/local"
+# By default, we install to /opt. This is because /usr/local/lib is not
+# included in the actual ld paths on many distributions. However, we make
+# due by installing a "fake" stub script that sets up LD_LIBRARY_PATH
+# with the proper variables into /usr/local/bin; this behavior can be
+# disabled, if desired.
+PREFIX="/opt/schism"
+INSTALL_USR_LOCAL_STUB=yes
+
+if test "x$1" = "x--help"; then
+	printf "usage: $0 [--help] [--no-usr-stub] [--prefix <prefix> (default: /opt/schism)>]"
+	exit
+fi
+
+# --no-usr-stub installs a stub script to /usr/local/bin
+# that sets up an environment to run the real binary
+if test "x$1" = "x--no-usr-stub"; then
+	INSTALL_USR_LOCAL_STUB=no
+	shift
+fi
+
+if test "x$1" = "x--prefix"; then
+	PREFIX="$2"
+	shift
+	shift
+fi
 
 SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 
-install "$SCRIPTPATH/schismtracker" "$PREFIX/bin/schismtracker"
+# mkdir -p is POSIX.
+mkdir -p "$PREFIX/bin" "$PREFIX/lib" "$PREFIX/share/applications" "$PREFIX/share/man/man1" "$PREFIX/share/pixmaps"
+
+cp "$SCRIPTPATH/schismtracker" "$PREFIX/bin/schismtracker"
 for i in FLAC.so.8 utf8proc.so.2.3.2 ogg.so.0; do
-	install "$SCRIPTPATH/lib$i" "$PREFIX/lib/lib$i"
+	cp "$SCRIPTPATH/lib$i" "$PREFIX/lib/lib$i"
 done
-install "$SCRIPTPATH/schism.desktop" "$PREFIX/share/applications/schism.desktop"
-install "$SCRIPTPATH/schismtracker.1" "$PREFIX/share/man/man1/schismtracker.1"
-install "$SCRIPTPATH/schism-icon-128.png" "$PREFIX/share/pixmaps/schism-icon-128.png"
+cp "$SCRIPTPATH/schism.desktop" "$PREFIX/share/applications/schism.desktop"
+cp "$SCRIPTPATH/schismtracker.1" "$PREFIX/share/man/man1/schismtracker.1"
+cp "$SCRIPTPATH/schism-icon-128.png" "$PREFIX/share/pixmaps/schism-icon-128.png"
+
+if test "x$INSTALL_USR_LOCAL_STUB" = "xyes" && ! test "x$PREFIX" = "x/usr/local"; then
+	cat<<EOF > "/usr/local/bin/schismtracker"
+#!/bin/sh
+
+env LD_LIBRARY_PATH="$PREFIX/lib" "$PREFIX/bin/schismtracker"
+EOF
+	# don't forget!
+	chmod +x "/usr/local/bin/schismtracker"
+fi
