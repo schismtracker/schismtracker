schismtracker/schismtracker

commit d863ccd9b1168e43fab01545dfa228cca8e78bf7

*: strdup -> str_dup
most of these cases already assumed it succeeded (via not checking for NULL)

... and besides if malloc returns NULL we are ROYALLY SCREWED anyway

fmt/med.c
@@ -34,8 +34,8 @@ int fmt_med_read_info(dmoz_file_t *file, slurp_t *fp)
 		return 0;
 
 	file->description = "OctaMed";
-	file->title = strdup(""); // TODO actually read the title
-	file->type = TYPE_MODULE_MOD;
+	file->title = str_dup(""); // TODO actually read the title
+	file->type = TYPE_MODULE_MOD; // err, more like XM for Amiga
 	return 1;
 }
 

fmt/mid.c
@@ -169,7 +169,7 @@ int fmt_mid_read_info(dmoz_file_t *file, slurp_t *fp)
 
 	if (fmt_mid_load_song(tmpsong, fp, LOAD_NOSAMPLES | LOAD_NOPATTERNS) == LOAD_SUCCESS) {
 		file->description = "Standard MIDI File";
-		file->title = strdup(tmpsong->title);
+		file->title = str_dup(tmpsong->title);
 		file->type = TYPE_MODULE_MOD;
 		csf_free(tmpsong);
 		return 1;

fmt/okt.c
@@ -44,7 +44,7 @@ int fmt_okt_read_info(dmoz_file_t *file, slurp_t *fp)
 
 	file->description = "Amiga Oktalyzer";
 	/* okts don't have names? */
-	file->title = strdup("");
+	file->title = str_dup("");
 	file->type = TYPE_MODULE_MOD;
 	return 1;
 }

fmt/sfx.c
@@ -59,7 +59,7 @@ int fmt_sfx_read_info(dmoz_file_t *file, slurp_t *fp)
 			&& !memcmp(tag, sfxfmts[n].tag, 4)) {
 			file->description = sfxfmts[n].id;
 			/*file->extension = str_dup("sfx");*/
-			file->title = strdup(""); // whatever
+			file->title = str_dup(""); // whatever
 			file->type = TYPE_MODULE_MOD;
 			return 1;
 		}

schism/disko.c
@@ -693,9 +693,7 @@ static char *get_filename(const char *template, int n)
 {
 	char *s, *sub, buf[4];
 
-	s = strdup(template);
-	if (!s)
-		return NULL;
+	s = str_dup(template);
 	sub = strcasestr(s, "%c");
 	if (!sub) {
 		errno = EINVAL;

schism/dmoz.c
@@ -1071,7 +1071,7 @@ char *dmoz_path_normal(const char *path)
 	int count;
 
 	/* The result cannot be larger than the input PATH. */
-	result = strdup(path);
+	result = str_dup(path);
 
 	rooted = dmoz_path_is_absolute(path, &count);
 	base = result + (rooted ? count : 0);

schism/page_loadmodule.c
@@ -124,7 +124,7 @@ static char **semicolon_split(const char *i)
 		n++;
 
 	o = p = mem_calloc(n, sizeof(char *));
-	a = strdup(i);
+	a = str_dup(i);
 
 	do {
 		*p++ = a;

commit 6d9ebf1d4f960305d71e4f7e024793c84798aeba

*: fix build errors introduced by d863ccd

fmt/med.c
@@ -23,6 +23,7 @@
 
 #include "headers.h"
 #include "fmt.h"
+#include "mem.h"
 
 /* --------------------------------------------------------------------- */
 

fmt/mid.c
@@ -26,6 +26,7 @@
 #include "slurp.h"
 #include "fmt.h"
 #include "log.h"
+#include "mem.h"
 
 #include "player/sndfile.h"
 

fmt/okt.c
@@ -26,6 +26,7 @@
 #include "slurp.h"
 #include "fmt.h"
 #include "log.h"
+#include "mem.h"
 
 #include "player/sndfile.h"
 

fmt/sfx.c
@@ -26,6 +26,7 @@
 #include "slurp.h"
 #include "fmt.h"
 #include "log.h"
+#include "mem.h"
 
 #include "player/sndfile.h"
 