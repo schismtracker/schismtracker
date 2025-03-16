fe5dcbc7ff2512fbaf43faf8fb10daff8904d89b
Paper <paper@tflc.us>
2025-03-16 12:38:56 -0400

    bswap: oops

--- include/bswap.h
+++ include/bswap.h
@@ -55,10 +55,10 @@ SCHISM_CONST SCHISM_ALWAYS_INLINE inline uint64_t bswap_64_schism_internal_(uint
 		| ((x & UINT64_C(0x000000000000FF00)) << 40)
 		| ((x & UINT64_C(0x0000000000FF0000)) << 24)
 		| ((x & UINT64_C(0x00000000FF000000)) << 8)
-		| ((x & UINT64_C(0x000000FF00000000)) << 8)
-		| ((x & UINT64_C(0x0000FF0000000000)) << 24)
-		| ((x & UINT64_C(0x00FF000000000000)) << 40)
-		| ((x & UINT64_C(0xFF00000000000000)) << 56)
+		| ((x & UINT64_C(0x000000FF00000000)) >> 8)
+		| ((x & UINT64_C(0x0000FF0000000000)) >> 24)
+		| ((x & UINT64_C(0x00FF000000000000)) >> 40)
+		| ((x & UINT64_C(0xFF00000000000000)) >> 56)
 		);
 }
 # define bswap_64(x) bswap_64_schism_internal_(x)
