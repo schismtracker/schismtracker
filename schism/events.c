7a74a5237db1a73536e43dbcace57297d7ed53c4
Jonathan Gilbert <logic@deltaq.org>
2025-05-27 15:50:49 -0500

    Added a mechanism to translate a key as specified for the QWERTY keyboard to the current keyboard layout: - Added back-end method get_key_name_from_scancode to events.h. Implemented it in sys/sdl2/events.c and sys/sdl3/events.c. Provided a fallback in sys/sdl12/events.c that is just a passthrough that returns the same key, since SDL 1.2 doesn't expose keyboard layout information. - Added method events_describe_physical_key_for_qwerty_key to events.c/events.h.

--- schism/events.c
+++ schism/events.c
@@ -210,3 +210,104 @@ schism_keymod_t events_get_keymod_state(void)
 {
 	return (events_backend ? events_backend->keymod_state() : 0);
 }
+
+const char *events_describe_physical_key_for_qwerty_key(char qwerty_key)
+{
+	schism_keysym_t keysym = (schism_keysym_t)qwerty_key;
+	schism_scancode_t scancode;
+	int shifted = 0;
+
+	const char *key_name;
+
+	static char description[50];
+
+	switch (keysym) {
+		case SCHISM_KEYSYM_UNKNOWN: scancode = SCHISM_SCANCODE_UNKNOWN; break;
+
+		case SCHISM_KEYSYM_RETURN: scancode = SCHISM_SCANCODE_RETURN; break;
+		case SCHISM_KEYSYM_ESCAPE: scancode = SCHISM_SCANCODE_ESCAPE; break;
+		case SCHISM_KEYSYM_BACKSPACE: scancode = SCHISM_SCANCODE_BACKSPACE; break;
+		case SCHISM_KEYSYM_TAB: scancode = SCHISM_SCANCODE_TAB; break;
+		case SCHISM_KEYSYM_SPACE: scancode = SCHISM_SCANCODE_SPACE; break;
+		case SCHISM_KEYSYM_EXCLAIM: scancode = SCHISM_SCANCODE_1; shifted = 1; break;
+		case SCHISM_KEYSYM_QUOTEDBL: scancode = SCHISM_SCANCODE_APOSTROPHE; shifted = 1; break;
+		case SCHISM_KEYSYM_HASH: scancode = SCHISM_SCANCODE_3; shifted = 1; break;
+		case SCHISM_KEYSYM_PERCENT: scancode = SCHISM_SCANCODE_5; shifted = 1; break;
+		case SCHISM_KEYSYM_DOLLAR: scancode = SCHISM_SCANCODE_4; shifted = 1; break;
+		case SCHISM_KEYSYM_AMPERSAND: scancode = SCHISM_SCANCODE_7; shifted = 1; break;
+		case SCHISM_KEYSYM_QUOTE: scancode = SCHISM_SCANCODE_APOSTROPHE; break;
+		case SCHISM_KEYSYM_LEFTPAREN: scancode = SCHISM_SCANCODE_9; shifted = 1; break;
+		case SCHISM_KEYSYM_RIGHTPAREN: scancode = SCHISM_SCANCODE_0; shifted = 1; break;
+		case SCHISM_KEYSYM_ASTERISK: scancode = SCHISM_SCANCODE_8; shifted = 1; break;
+		case SCHISM_KEYSYM_PLUS: scancode = SCHISM_SCANCODE_EQUALS; shifted = 1; break;
+		case SCHISM_KEYSYM_COMMA: scancode = SCHISM_SCANCODE_COMMA; break;
+		case SCHISM_KEYSYM_MINUS: scancode = SCHISM_SCANCODE_MINUS; break;
+		case SCHISM_KEYSYM_PERIOD: scancode = SCHISM_SCANCODE_PERIOD; break;
+		case SCHISM_KEYSYM_SLASH: scancode = SCHISM_SCANCODE_SLASH; break;
+		case SCHISM_KEYSYM_0: scancode = SCHISM_SCANCODE_0; break;
+		case SCHISM_KEYSYM_1: scancode = SCHISM_SCANCODE_1; break;
+		case SCHISM_KEYSYM_2: scancode = SCHISM_SCANCODE_2; break;
+		case SCHISM_KEYSYM_3: scancode = SCHISM_SCANCODE_3; break;
+		case SCHISM_KEYSYM_4: scancode = SCHISM_SCANCODE_4; break;
+		case SCHISM_KEYSYM_5: scancode = SCHISM_SCANCODE_5; break;
+		case SCHISM_KEYSYM_6: scancode = SCHISM_SCANCODE_6; break;
+		case SCHISM_KEYSYM_7: scancode = SCHISM_SCANCODE_7; break;
+		case SCHISM_KEYSYM_8: scancode = SCHISM_SCANCODE_8; break;
+		case SCHISM_KEYSYM_9: scancode = SCHISM_SCANCODE_9; break;
+		case SCHISM_KEYSYM_COLON: scancode = SCHISM_SCANCODE_SEMICOLON; shifted = 1; break;
+		case SCHISM_KEYSYM_SEMICOLON: scancode = SCHISM_SCANCODE_SEMICOLON; break;
+		case SCHISM_KEYSYM_LESS: scancode = SCHISM_SCANCODE_COMMA; shifted = 1; break;
+		case SCHISM_KEYSYM_EQUALS: scancode = SCHISM_SCANCODE_EQUALS; break;
+		case SCHISM_KEYSYM_GREATER: scancode = SCHISM_SCANCODE_PERIOD; shifted = 1; break;
+		case SCHISM_KEYSYM_QUESTION: scancode = SCHISM_SCANCODE_SLASH; shifted = 1; break;
+		case SCHISM_KEYSYM_AT: scancode = SCHISM_SCANCODE_2; shifted = 1; break;
+
+		case SCHISM_KEYSYM_LEFTBRACKET: scancode = SCHISM_SCANCODE_LEFTBRACKET; break;
+		case SCHISM_KEYSYM_BACKSLASH: scancode = SCHISM_SCANCODE_BACKSLASH; break;
+		case SCHISM_KEYSYM_RIGHTBRACKET: scancode = SCHISM_SCANCODE_RIGHTBRACKET; break;
+		case SCHISM_KEYSYM_CARET: scancode = SCHISM_SCANCODE_6; shifted = 1; break;
+		case SCHISM_KEYSYM_UNDERSCORE: scancode = SCHISM_SCANCODE_MINUS; shifted = 1; break;
+		case SCHISM_KEYSYM_BACKQUOTE: scancode = SCHISM_SCANCODE_GRAVE; break;
+		case SCHISM_KEYSYM_a: scancode = SCHISM_SCANCODE_A; break;
+		case SCHISM_KEYSYM_b: scancode = SCHISM_SCANCODE_B; break;
+		case SCHISM_KEYSYM_c: scancode = SCHISM_SCANCODE_C; break;
+		case SCHISM_KEYSYM_d: scancode = SCHISM_SCANCODE_D; break;
+		case SCHISM_KEYSYM_e: scancode = SCHISM_SCANCODE_E; break;
+		case SCHISM_KEYSYM_f: scancode = SCHISM_SCANCODE_F; break;
+		case SCHISM_KEYSYM_g: scancode = SCHISM_SCANCODE_G; break;
+		case SCHISM_KEYSYM_h: scancode = SCHISM_SCANCODE_H; break;
+		case SCHISM_KEYSYM_i: scancode = SCHISM_SCANCODE_I; break;
+		case SCHISM_KEYSYM_j: scancode = SCHISM_SCANCODE_J; break;
+		case SCHISM_KEYSYM_k: scancode = SCHISM_SCANCODE_K; break;
+		case SCHISM_KEYSYM_l: scancode = SCHISM_SCANCODE_L; break;
+		case SCHISM_KEYSYM_m: scancode = SCHISM_SCANCODE_M; break;
+		case SCHISM_KEYSYM_n: scancode = SCHISM_SCANCODE_N; break;
+		case SCHISM_KEYSYM_o: scancode = SCHISM_SCANCODE_O; break;
+		case SCHISM_KEYSYM_p: scancode = SCHISM_SCANCODE_P; break;
+		case SCHISM_KEYSYM_q: scancode = SCHISM_SCANCODE_Q; break;
+		case SCHISM_KEYSYM_r: scancode = SCHISM_SCANCODE_R; break;
+		case SCHISM_KEYSYM_s: scancode = SCHISM_SCANCODE_S; break;
+		case SCHISM_KEYSYM_t: scancode = SCHISM_SCANCODE_T; break;
+		case SCHISM_KEYSYM_u: scancode = SCHISM_SCANCODE_U; break;
+		case SCHISM_KEYSYM_v: scancode = SCHISM_SCANCODE_V; break;
+		case SCHISM_KEYSYM_w: scancode = SCHISM_SCANCODE_W; break;
+		case SCHISM_KEYSYM_x: scancode = SCHISM_SCANCODE_X; break;
+		case SCHISM_KEYSYM_y: scancode = SCHISM_SCANCODE_Y; break;
+		case SCHISM_KEYSYM_z: scancode = SCHISM_SCANCODE_Z; break;
+
+		case SCHISM_KEYSYM_CAPSLOCK: scancode = SCHISM_SCANCODE_CAPSLOCK; break;
+
+		case SCHISM_KEYSYM_DELETE: scancode = SCHISM_SCANCODE_DELETE; break;
+	}
+
+	key_name = events_backend->get_key_name_from_scancode(scancode);
+
+	if (!shifted)
+		return key_name;
+	else {
+		sprintf(description, "Shift-%s", key_name);
+
+		return description;
+	}
+}
+
