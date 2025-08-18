600c19093218b77ca80582cf610f82ae1bdaefe4
Jonathan Gilbert <logic@deltaq.org>
2025-08-18 12:10:18 -0500

    fmt/med: implemented reading song title (#764)
    * fmt/med: implemented reading song title
    
    * fmt/med: PR feedback
    Updated fmt_med_read_info to not truncate the name; that's the caller's job.
    Reworked fmt_med_read_info to read only the fields needed. Left the structure definitions on the offchance that some future programmer wants to do a full OctaMed loader.
    Fixed up data types in fmt_med_read_info.

--- fmt/med.c
+++ fmt/med.c
@@ -25,17 +25,104 @@
 #include "fmt.h"
 #include "mem.h"
 
+#define MMD0_ID "MMD0"
+
+struct MMD0
+{
+	/* 0 */  char     id[4];
+	/* 4 */  uint32_t modlen;
+	/* 8 */  uint32_t song_ptr; /* struct MMD0song */
+	/* 12 */ uint16_t psecnum;    /* for the player routine, MMD2 only */
+	/* 14 */ uint16_t pseq;       /*  "   "   "   "    */
+	/* 16 */ uint32_t blockarr_ptr; /* struct MMD0Block *[] (array of pointers) */
+	/* 20 */ uint8_t  mmdflags;
+	/* 21 */ uint8_t  reserved[3];
+	/* 24 */ uint32_t smplarr_ptr; /* struct InstrHdr *[] (array of pointers) */
+	/* 28 */ uint32_t reserved2;
+	/* 32 */ uint32_t expdata_ptr; /* struct MMD0exp <-- song name is hiding in here */
+	/* 36 */ uint32_t reserved3;
+	/* 40 */ uint16_t pstate;  /* some data for the player routine */
+	/* 42 */ uint16_t pblock;
+	/* 44 */ uint16_t pline;
+	/* 46 */ uint16_t pseqnum;
+	/* 48 */ int16_t  actplayline;
+	/* 50 */ uint8_t  counter;
+	/* 51 */ uint8_t  extra_songs; /* number of songs - 1 */
+};
+
+struct MMD0exp
+{
+	/*  0 */ uint32_t nextmod_ptr; /* struct MMD0 */
+	/*  4 */ uint32_t exp_smp_ptr; /* struct InstrExp */
+	/*  8 */ uint16_t s_ext_entries;
+	/* 10 */ uint16_t s_ext_entrsz;
+	/* 12 */ uint32_t annotxt_ptr; /* char[] */
+	/* 16 */ uint32_t annolen;
+	/* 20 */ uint32_t iinfo_ptr; /* struct MMDInstrInfo */
+	/* 24 */ uint16_t i_ext_entries;
+	/* 26 */ uint16_t i_ext_entrsz;
+	/* 28 */ uint32_t jumpmask;
+	/* 32 */ uint32_t rgbtable_ptr; /* uint16_t[] */
+	/* 36 */ uint8_t  channelsplit[4];
+	/* 40 */ uint32_t n_info_ptr /* struct NotationInfo */;
+	/* 44 */ uint32_t songname_ptr; /* char[] */
+	/* 48 */ uint32_t songnamelen;
+	/* 52 */ uint32_t dumps_ptr; /* struct MMDDumpData */
+	/* 56 */ uint32_t mmdinfo_ptr; /* struct MMDInfo */
+	/* 60 */ uint32_t mmdrexx_ptr; /* struct MMDARexx */
+	/* 64 */ uint32_t mmdcmd3x_ptr; /* struct MMDMIDICmd3x */
+	/* 68 */ uint32_t reserved2[3];
+	/* 80 */ uint32_t tag_end;
+};
+
 /* --------------------------------------------------------------------- */
 
 int fmt_med_read_info(dmoz_file_t *file, slurp_t *fp)
 {
-	unsigned char magic[4];
-	if (slurp_read(fp, magic, sizeof(magic)) != sizeof(magic)
-		|| memcmp(magic, "MMD0", 4))
+	struct MMD0 hdr;
+	struct MMD0exp exp;
+
+	uint64_t length = slurp_length(fp);
+
+	if (length < 52) /* sizeof(hdr) */
 		return 0;
 
+	if ((slurp_read(fp, hdr.id, 4) != 4)
+	 || memcmp(hdr.id, MMD0_ID, 4))
+		return 0;
+
+	if (slurp_seek(fp, 32, SEEK_SET)
+	 || (slurp_read(fp, &hdr.expdata_ptr, sizeof(hdr.expdata_ptr)) != sizeof(hdr.expdata_ptr)))
+		return 0;
+
+	uint32_t exp_struct_ptr = bswapBE32(hdr.expdata_ptr);
+
+	if ((exp_struct_ptr + sizeof(struct MMD0exp) >= length)
+	 || slurp_seek(fp, exp_struct_ptr + 44, SEEK_SET)
+	 || (slurp_read(fp, &exp.songname_ptr, sizeof(exp.songname_ptr)) != sizeof(exp.songname_ptr))
+	 || (slurp_read(fp, &exp.songnamelen, sizeof(exp.songnamelen)) != sizeof(exp.songnamelen)))
+		return 0;
+
+	uint32_t name_ptr = bswapBE32(exp.songname_ptr);
+	uint32_t name_len = bswapBE32(exp.songnamelen);
+
+	char *name_buffer = mem_alloc(name_len + 1);
+
+	if (!name_buffer)
+		return 0;
+
+	name_buffer[name_len] = 0;
+
+	if ((name_ptr + name_len >= length)
+	 || slurp_seek(fp, name_ptr, SEEK_SET)
+	 || (slurp_read(fp, name_buffer, name_len) != name_len))
+	{
+		free(name_buffer);
+		return 0;
+	}
+
 	file->description = "OctaMed";
-	file->title = str_dup(""); // TODO actually read the title
+	file->title = name_buffer;
 	file->type = TYPE_MODULE_MOD; // err, more like XM for Amiga
 	return 1;
 }
