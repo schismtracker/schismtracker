3a4c2af6ec30b2b8236a0ab77c165d6b88685635
Jonathan Gilbert <logic@deltaq.org>
2025-08-28 16:03:55 -0500

    Revert "test/cases/str: convert array-based tests to be more like test_str_from_num_thousands"
    This reverts commit f78ef5e5a150eba7895de86f933064a30dbf4988.

--- test/cases/str.c
+++ test/cases/str.c
@@ -41,180 +41,288 @@ static testresult_t test_str_from_num_thousands(int32_t n, const char *expect)
 	RETURN_PASS;
 }
 
-#define TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(name, num, expect) \
-    TEST_CASE_STUB(str_from_num_thousands_##name, test_str_from_num_thousands, num, expect)
+testresult_t test_str_from_num_thousands_0(void)
+{
+	return test_str_from_num_thousands(0, "0");
+}
 
-/* wow, sublime text totally fails at syntax highlighting here */
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(0, INT32_C(0), "0")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(999, INT32_C(999), "999")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(1_000, INT32_C(1000), "1,000")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(999_999, INT32_C(999999), "999,999")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(1_000_000, INT32_C(1000000), "1,000,000")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(999_999_999, INT32_C(999999999), "999,999,999")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(1_000_000_000, INT32_C(1000000000), "1,000,000,000")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(int32_min, INT32_C(INT32_MIN), "-2,147,483,648")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(int32_max, INT32_C(INT32_MAX), "2,147,483,647")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_1_positive, INT32_C(7), "7")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_2_positive, INT32_C(32), "32")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_3_positive, INT32_C(891), "891")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_4_positive, INT32_C(5834), "5,834")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_5_positive, INT32_C(45891), "45,891")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_6_positive, INT32_C(591848), "591,848")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_7_positive, INT32_C(5691834), "5,691,834")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_8_positive, INT32_C(68917829), "68,917,829")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_9_positive, INT32_C(591848968), "591,848,968")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_10_positive, INT32_C(2082850450), "2,082,850,450")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_1_negative, INT32_C(-7), "-7")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_2_negative, INT32_C(-32), "-32")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_3_negative, INT32_C(-891), "-891")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_4_negative, INT32_C(-5834), "-5,834")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_5_negative, INT32_C(-45891), "-45,891")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_6_negative, INT32_C(-591848), "-591,848")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_7_negative, INT32_C(-5691834), "-5,691,834")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_8_negative, INT32_C(-68917829), "-68,917,829")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_9_negative, INT32_C(-591848968), "-591,848,968")
-TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE(digits_10_negative, INT32_C(-2082850450), "-2,082,850,450")
+testresult_t test_str_from_num_thousands_999(void)
+{
+	return test_str_from_num_thousands(999, "999");
+}
 
-#undef TEST_STR_FROM_NUM_THOUSANDS_TEMPLATE
+testresult_t test_str_from_num_thousands_1_000(void)
+{
+	return test_str_from_num_thousands(1000, "1,000");
+}
+
+testresult_t test_str_from_num_thousands_999_999(void)
+{
+	return test_str_from_num_thousands(999999, "999,999");
+}
+
+testresult_t test_str_from_num_thousands_1_000_000(void)
+{
+	return test_str_from_num_thousands(1000000, "1,000,000");
+}
+
+testresult_t test_str_from_num_thousands_999_999_999(void)
+{
+	return test_str_from_num_thousands(999999999, "999,999,999");
+}
+
+testresult_t test_str_from_num_thousands_1_000_000_000(void)
+{
+	return test_str_from_num_thousands(1000000000, "1,000,000,000");
+}
+
+testresult_t test_str_from_num_thousands_int32_min(void)
+{
+	return test_str_from_num_thousands(INT32_MIN, "-2,147,483,648");
+}
+
+testresult_t test_str_from_num_thousands_int32_max(void)
+{
+	return test_str_from_num_thousands(INT32_MAX, "2,147,483,647");
+}
+
+testresult_t test_str_from_num_thousands_digits_1_positive(void)
+{
+	return test_str_from_num_thousands(7, "7");
+}
+
+testresult_t test_str_from_num_thousands_digits_2_positive(void)
+{
+	return test_str_from_num_thousands(32, "32");
+}
+
+testresult_t test_str_from_num_thousands_digits_3_positive(void)
+{
+	return test_str_from_num_thousands(891, "891");
+}
+
+testresult_t test_str_from_num_thousands_digits_4_positive(void)
+{
+	return test_str_from_num_thousands(5834, "5,834");
+}
+
+testresult_t test_str_from_num_thousands_digits_5_positive(void)
+{
+	return test_str_from_num_thousands(45891, "45,891");
+}
+
+testresult_t test_str_from_num_thousands_digits_6_positive(void)
+{
+	return test_str_from_num_thousands(591848, "591,848");
+}
+
+testresult_t test_str_from_num_thousands_digits_7_positive(void)
+{
+	return test_str_from_num_thousands(5691834, "5,691,834");
+}
+
+testresult_t test_str_from_num_thousands_digits_8_positive(void)
+{
+	return test_str_from_num_thousands(68917829, "68,917,829");
+}
+
+testresult_t test_str_from_num_thousands_digits_9_positive(void)
+{
+	return test_str_from_num_thousands(591848968, "591,848,968");
+}
+
+testresult_t test_str_from_num_thousands_digits_10_positive(void)
+{
+	return test_str_from_num_thousands(2082850450, "2,082,850,450");
+}
+
+testresult_t test_str_from_num_thousands_digits_1_negative(void)
+{
+	return test_str_from_num_thousands(-7, "-7");
+}
+
+testresult_t test_str_from_num_thousands_digits_2_negative(void)
+{
+	return test_str_from_num_thousands(-32, "-32");
+}
+
+testresult_t test_str_from_num_thousands_digits_3_negative(void)
+{
+	return test_str_from_num_thousands(-891, "-891");
+}
+
+testresult_t test_str_from_num_thousands_digits_4_negative(void)
+{
+	return test_str_from_num_thousands(-5834, "-5,834");
+}
+
+testresult_t test_str_from_num_thousands_digits_5_negative(void)
+{
+	return test_str_from_num_thousands(-45891, "-45,891");
+}
+
+testresult_t test_str_from_num_thousands_digits_6_negative(void)
+{
+	return test_str_from_num_thousands(-591848, "-591,848");
+}
+
+testresult_t test_str_from_num_thousands_digits_7_negative(void)
+{
+	return test_str_from_num_thousands(-5691834, "-5,691,834");
+}
+
+testresult_t test_str_from_num_thousands_digits_8_negative(void)
+{
+	return test_str_from_num_thousands(-68917829, "-68,917,829");
+}
+
+testresult_t test_str_from_num_thousands_digits_9_negative(void)
+{
+	return test_str_from_num_thousands(-591848968, "-591,848,968");
+}
+
+testresult_t test_str_from_num_thousands_digits_10_negative(void)
+{
+	return test_str_from_num_thousands(-2082850450, "-2,082,850,450");
+}
 
 /* ------------------------------------------------------------------------ */
 
-static testresult_t test_str_concat_common(const char *expected,
-	const char *arg1, const char *arg2, const char *arg3, const char *arg4,
-	const char *arg5)
+testresult_t test_str_concat(void)
 {
+	static const struct {
+		const char *args[5];
+		const char *expected;
+	} c[] = {
+		{{"123", "456", "789", "012"}, "123456789012"},
+		{{"123", "ok", "67"}, "123ok67"},
+	};
+	size_t i;
 	char *r;
 
-	r = str_concat(arg1, arg2, arg3, arg4, arg5, (char *)NULL);
-	REQUIRE(r);
+	for (i = 0; i < ARRAY_SIZE(c); i++) {
+		r = str_concat(c[i].args[0], c[i].args[1], c[i].args[2], c[i].args[3], c[i].args[4], (char *)NULL);
+		REQUIRE(r);
 
-	ASSERT_PRINTF(!strcmp(expected, r), "result \"%s\" with args "
-		"(\"%s\", \"%s\", \"%s\", \"%s\", \"%s\") was not \"%s\" as expected",
-		r,
-		(arg1) ? (arg1) : "NULL",
-		(arg2) ? (arg2) : "NULL",
-		(arg3) ? (arg3) : "NULL",
-		(arg4) ? (arg4) : "NULL",
-		(arg5) ? (arg5) : "NULL",
-		expected);
+		ASSERT_PRINTF(!strcmp(c[i].expected, r), "result \"%s\" with args "
+			"(\"%s\", \"%s\", \"%s\", \"%s\", \"%s\") was not \"%s\" as expected",
+			r,
+			c[i].args[0] ? c[i].args[0] : "NULL",
+			c[i].args[1] ? c[i].args[1] : "NULL",
+			c[i].args[2] ? c[i].args[2] : "NULL",
+			c[i].args[3] ? c[i].args[3] : "NULL",
+			c[i].args[4] ? c[i].args[4] : "NULL",
+			c[i].expected);
 
-	free(r);
+		free(r);
+	}
 
 	RETURN_PASS;
 }
-
-#define TEST_STR_CONCAT_TEMPLATE(name, expect, arg1, arg2, arg3, arg4, arg5) \
-    TEST_CASE_STUB(str_concat_##name, test_str_concat_common, expect, arg1, arg2, arg3, arg4, arg5)
-
-TEST_STR_CONCAT_TEMPLATE(123456789012, "123456789012", "123", "456", "789", "012", NULL)
-TEST_STR_CONCAT_TEMPLATE(123ok67, "123ok67", "123", "ok", "67", NULL, NULL)
-
-#undef TEST_STR_CONCAT_TEMPLATE
 
 /* ------------------------------------------------------------------------ */
 
-static testresult_t test_str_from_num_common(int digits, uint32_t num,
-	const char *expect)
+testresult_t test_str_from_num(void)
 {
-	char buf[11];
-	char *r;
+	static const struct {
+		int digits;
+		uint32_t num;
+		const char *result;
+	} c[] = {
+		{0, UINT32_C(1),          "1"},
+		{0, UINT32_C(10),         "10"},
+		{0, UINT32_C(100),        "100"},
+		{0, UINT32_C(1000),       "1000"},
+		{0, UINT32_C(10000),      "10000"},
+		{0, UINT32_C(100000),     "100000"},
+		{0, UINT32_C(1000000),    "1000000"},
+		{0, UINT32_C(10000000),   "10000000"},
+		{0, UINT32_C(100000000),  "100000000"},
+		{0, UINT32_C(1000000000), "1000000000"},
+		{2, UINT32_C(1),          "01"},
+		{4, UINT32_C(5),          "0005"},
+		{9, UINT32_C(9),          "000000009"},
+	};
+	size_t i;
 
-	r = str_from_num(digits, num, buf);
+	for (i = 0; i < ARRAY_SIZE(c); i++) {
+		char buf[11];
 
-	ASSERT_PRINTF(r == &buf[0], "return value \"%p\" is not the same "
-		"as input pointer \"%p\"", r, buf);
-	ASSERT_PRINTF(!strcmp(expect, buf), "result with integer %" PRIu32
-		" was \"%s\", not \"%s\" as expected", num, buf, expect);
+		str_from_num(c[i].digits, c[i].num, buf);
+
+		ASSERT_PRINTF(!strcmp(c[i].result, buf), "result with integer %" PRIu32
+			" was \"%s\", not \"%s\" as expected", c[i].num, buf, c[i].result);
+	}
 
 	RETURN_PASS;
 }
 
-#define TEST_STR_FROM_NUM_TEMPLATE(name, digits, num, expect) \
-    TEST_CASE_STUB(str_from_num_##name, test_str_from_num_common, digits, num, expect)
+testresult_t test_str_from_num_signed(void)
+{
+	static const struct {
+		int digits;
+		int32_t num;
+		const char *result;
+	} c[] = {
+		{0, INT32_C(1),          "1"},
+		{0, INT32_C(10),         "10"},
+		{0, INT32_C(100),        "100"},
+		{0, INT32_C(1000),       "1000"},
+		{0, INT32_C(10000),      "10000"},
+		{0, INT32_C(100000),     "100000"},
+		{0, INT32_C(1000000),    "1000000"},
+		{0, INT32_C(10000000),   "10000000"},
+		{0, INT32_C(100000000),  "100000000"},
+		{0, INT32_C(1000000000), "1000000000"},
+		{2, INT32_C(1),          "01"},
+		{4, INT32_C(5),          "0005"},
+		{9, INT32_C(9),          "000000009"},
+		{3, INT32_C(-9),         "-09"},
+		{5, INT32_C(-9),         "-0009"},
+		{9, INT32_C(-9),         "-00000009"},
+	};
+	size_t i;
 
-TEST_STR_FROM_NUM_TEMPLATE(0_1, 0, UINT32_C(1),          "1")
-TEST_STR_FROM_NUM_TEMPLATE(0_10, 0, UINT32_C(10),         "10")
-TEST_STR_FROM_NUM_TEMPLATE(0_100, 0, UINT32_C(100),        "100")
-TEST_STR_FROM_NUM_TEMPLATE(0_1000, 0, UINT32_C(1000),       "1000")
-TEST_STR_FROM_NUM_TEMPLATE(0_10000, 0, UINT32_C(10000),      "10000")
-TEST_STR_FROM_NUM_TEMPLATE(0_100000, 0, UINT32_C(100000),     "100000")
-TEST_STR_FROM_NUM_TEMPLATE(0_1000000, 0, UINT32_C(1000000),    "1000000")
-TEST_STR_FROM_NUM_TEMPLATE(0_10000000, 0, UINT32_C(10000000),   "10000000")
-TEST_STR_FROM_NUM_TEMPLATE(0_100000000, 0, UINT32_C(100000000),  "100000000")
-TEST_STR_FROM_NUM_TEMPLATE(0_1000000000, 0, UINT32_C(1000000000), "1000000000")
-TEST_STR_FROM_NUM_TEMPLATE(2_1, 2, UINT32_C(1),          "01")
-TEST_STR_FROM_NUM_TEMPLATE(4_5, 4, UINT32_C(5),          "0005")
-TEST_STR_FROM_NUM_TEMPLATE(9_9, 9, UINT32_C(9),          "000000009")
+	for (i = 0; i < ARRAY_SIZE(c); i++) {
+		char buf[12];
 
-#undef TEST_STR_FROM_NUM_TEMPLATE
+		str_from_num_signed(c[i].digits, c[i].num, buf);
+
+		ASSERT_PRINTF(!strcmp(c[i].result, buf), "result with integer %" PRId32
+			" was \"%s\", not \"%s\" as expected", c[i].num, buf, c[i].result);
+	}
+
+	RETURN_PASS;
+}
 
 /* ------------------------------------------------------------------------ */
 
-static testresult_t test_str_from_num_signed_common(int digits, int32_t num,
-	const char *expect)
+testresult_t test_str_get_num_lines(void)
 {
-	char buf[12];
-	char *r;
+	static const struct {
+		const char *text;
+		int result;
+	} c[] = {
+		/* stress test */
+		{"wow\n", 1},
+		{"wow", 0 /* wait, what? */},
+		{"wow\r\n", 1},
+		{"wow\r\nhai\n", 2},
+		{"nice\n\r\n", 2},
+		{"awesome\r\n\n", 2},
+	};
 
-	r = str_from_num_signed(digits, num, buf);
+	size_t i;
 
-	ASSERT_PRINTF(r == &buf[0], "return value \"%p\" is not the same "
-		"as input pointer \"%p\"", r, buf);
-	ASSERT_PRINTF(!strcmp(expect, buf), "result with integer %" PRId32
-		" was \"%s\", not \"%s\" as expected", num, buf, expect);
+	for (i = 0; i < ARRAY_SIZE(c); i++) {
+		int x;
+
+		x = str_get_num_lines(c[i].text);
+
+		ASSERT_PRINTF(x == c[i].result, "result for string \"%s\" was %d, not %d as expected", c[i].text, x, c[i].result);
+	}
 
 	RETURN_PASS;
 }
-
-#define TEST_STR_FROM_NUM_SIGNED_TEMPLATE(name, digits, num, expect) \
-    TEST_CASE_STUB(str_from_num_signed_##name, \
-    	test_str_from_num_signed_common, digits, num, expect)
-
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_1, 0, INT32_C(1),          "1")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_10, 0, INT32_C(10),         "10")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_100, 0, INT32_C(100),        "100")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_1000, 0, INT32_C(1000),       "1000")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_10000, 0, INT32_C(10000),      "10000")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_100000, 0, INT32_C(100000),     "100000")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_1000000, 0, INT32_C(1000000),    "1000000")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_10000000, 0, INT32_C(10000000),   "10000000")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_100000000, 0, INT32_C(100000000),  "100000000")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(0_1000000000, 0, INT32_C(1000000000), "1000000000")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(2_1, 2, INT32_C(1),          "01")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(4_5, 4, INT32_C(5),          "0005")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(9_9, 9, INT32_C(9),          "000000009")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(3_neg9, 3, INT32_C(-9),         "-09")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(5_neg9, 5, INT32_C(-9),         "-0009")
-TEST_STR_FROM_NUM_SIGNED_TEMPLATE(9_neg9, 9, INT32_C(-9),         "-00000009")
-
-#undef TEST_STR_FROM_NUM_SIGNED_TEMPLATE
-
-/* ------------------------------------------------------------------------ */
-
-static testresult_t test_str_get_num_lines_common(const char *text,
-	int result)
-{
-	int x;
-
-	x = str_get_num_lines(text);
-
-	ASSERT_PRINTF(x == result, "result for string \"%s\" was %d, not %d as expected", text, x, result);
-
-	RETURN_PASS;
-}
-
-#define TEST_STR_GET_NUM_LINES_TEMPLATE(name, text, result) \
-    TEST_CASE_STUB(str_get_num_lines_##name, \
-    	test_str_get_num_lines_common, text, result)
-
-TEST_STR_GET_NUM_LINES_TEMPLATE(wow_LF, "wow\n", 1)
-TEST_STR_GET_NUM_LINES_TEMPLATE(wow, "wow", 0 /* wait, what? */)
-TEST_STR_GET_NUM_LINES_TEMPLATE(wow_CRLF, "wow\r\n", 1)
-TEST_STR_GET_NUM_LINES_TEMPLATE(wow_CRLF_hai_LF, "wow\r\nhai\n", 2)
-TEST_STR_GET_NUM_LINES_TEMPLATE(nice_LF_CRLF, "nice\n\r\n", 2)
-TEST_STR_GET_NUM_LINES_TEMPLATE(awesome_CRLF_LF, "awesome\r\n\n", 2)
-TEST_STR_GET_NUM_LINES_TEMPLATE(awesome_CR_hai_LF, "awesome\rhai\n", 2)
-
-#undef TEST_STR_GET_NUM_LINES_TEMPLATE
 
 /* TODO test the rest of the str functions */
