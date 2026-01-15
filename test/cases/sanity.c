3c732c2e32a6f4e204b6070c0cbf75d67ebc3a99
Paper <paper@tflc.us>
2026-01-15 17:15:20 -0500

    test/cases/sanity: print out extra info when things go wrong

--- test/cases/sanity.c
+++ test/cases/sanity.c
@@ -28,20 +28,56 @@
  * that everything works right. if not, there is probably a bug in
  * the OS or the libc. */
 
+static char *ex_strlcpy(char *dst, const char *src, size_t len)
+{
+	/* This is trivial to implement off of strncpy */
+	switch (len) {
+	default: /* len > 1 */
+		strncpy(dst, src, len-1);
+		SCHISM_FALLTHROUGH;
+	case 1:
+		dst[len-1] = 0;
+		SCHISM_FALLTHROUGH;
+	case 0:
+		return dst;
+	}
+}
+
 /* test_sanity_time: makes sure that
  *  time() -> localtime() -> mktime()
  * always returns back the same value for time_t */
 testresult_t test_sanity_time(void)
 {
-	time_t x;
+	time_t x, r;
 	struct tm t;
 
 	time(&x);
 
+	/* Note: this can actually fuck up if someone happens to
+	 * change the time zone between these two calls (race-y) */
 	localtime_r(&x, &t);
 
-	if (x != mktime(&t))
+	r = mktime(&t);
+
+	if (x != r) {
+		/* 64 chars ought to be enough for anybody */
+		char buf1[64], buf2[64], buf3[64];
+
+		ex_strlcpy(buf1, ctime(&x), sizeof(buf1));
+		ex_strlcpy(buf2, ctime(&r), sizeof(buf2));
+		ex_strlcpy(buf3, asctime(&t), sizeof(buf3));
+
+		test_log_printf(
+			" time(NULL) and mktime(localtime((time_t[]){ time(NULL); })) have different values!\n"
+			" something is very wrong.\n"
+			"\n"
+			" time(...): %s"
+			" localtime(...): %s"
+			" mktime(...): %s",
+			buf1, buf3, buf2
+		);
 		RETURN_FAIL;
+	}
 
 	RETURN_PASS;
 }
