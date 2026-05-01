08cacfbc7c28b92ac2a3789fc2d1b4bac75c0b96
Paper <paper@tflc.us>
2026-05-01 17:30:08 -0400

    config-parser: no longer unused

--- include/config-parser.h
+++ include/config-parser.h
@@ -60,7 +60,7 @@ typedef struct cfg_file cfg_file_t;
 /* --------------------------------------------------------------------------------------------------------- */
 /* public functions */
 
-int cfg_read_mem(const void *data, SCHISM_UNUSED size_t size, cfg_file_t *cfg);
+int cfg_read_mem(const void *data, size_t size, cfg_file_t *cfg);
 
 int cfg_read(cfg_file_t *cfg);
 
