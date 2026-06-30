14e508e4000988debeecc477d16df471784b1ce6
Jonathan Gilbert <logic@deltaq.org>
2026-06-30 14:15:48 -0500

    Collapsed coalescence of display_name for file->title in fmt_gstreamer_read_info.

--- fmt/gstreamer.c
+++ fmt/gstreamer.c
@@ -344,11 +344,7 @@ int fmt_gstreamer_read_info(dmoz_file_t *file, slurp_t *fp)
 							file->smp_speed = sample_rate;
 							file->smp_length = duration * sample_rate / GST_SECOND;
 
-							if (!display_name) {
-								display_name = file->base;
-							}
-
-							file->title = str_dup(display_name);
+							file->title = str_dup(display_name ? display_name : file->base);
 							file->description = description;
 
 							success = 1;
